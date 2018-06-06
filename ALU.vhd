71892b4ad5b07a1a2279506df886b0aa811d1070
harrylisby <harry@lisby.co>
2018-06-06 07:54:18 -0600

    Primera version publicada

---
+++ ALU.vhd
@@ -0,0 +1,125 @@
+library ieee;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+ENTITY ALU IS
+	PORT(	A: 	in	 	std_logic_vector(7 downto 0);
+			B: 	in 	std_logic_vector(7 downto 0);
+			Ci:	in 	std_logic;
+			R:		out 	std_logic_vector(7 downto 0);
+			Zo:	out 	std_logic;
+			Co: 	out 	std_logic;
+			Op:  	in 	std_logic_vector(3 downto 0);
+			HEX:	out 	std_logic_vector(27 downto 0)
+	);
+END ENTITY;
+	
+ARCHITECTURE A_ALU OF ALU IS
+							
+SIGNAL preR: std_logic_vector(8 downto 0);
+SIGNAL preCo: std_logic;
+
+BEGIN
+PROCESS(A,B,Ci,Op,preR,preCo)
+BEGIN
+	CASE Op IS
+		WHEN "0000" =>
+			preR <= "000000000";
+			HEX <= "1000000100000010000001000000";
+		WHEN "0001" =>
+			preR <= "000000000";
+			HEX <= "1000000100000010000001000000";
+		WHEN "0010" =>
+		--RLA <=					
+			preR(0) <= Ci;
+			preR(1) <= A(0);
+			preR(2) <= A(1);
+			preR(3) <= A(2);
+			preR(4) <= A(3);
+			preR(5) <= A(4);
+			preR(6) <= A(5);
+			preR(7) <= A(6);
+			preCo   <= A(7);
+			HEX <= "0101111100011111111110001000";
+		WHEN "0011" =>
+		--RRA =>
+			preCo   <= A(0);
+			preR(0) <= A(1);
+			preR(1) <= A(2);
+			preR(2) <= A(3);
+			preR(3) <= A(4);
+			preR(4) <= A(5);
+			preR(5) <= A(6);
+			preR(6) <= A(7);
+			preR(7) <= Ci;
+			HEX <= "0101111010111111111110001000";
+		WHEN "0100" =>
+		--A AND B
+			preR(7 downto 0) <= A AND B;
+			HEX <= "0001000111111101010110000011";
+			
+		WHEN "0101" =>
+		--A OR B
+			preR(7 downto 0) <= A OR B;
+			HEX <= "0001000010001101011110000011";
+			
+		WHEN "0110" =>
+		--A XOR B
+			preR(7 downto 0) <= A XOR B;
+			HEX <= "0001000000100101000110000011";
+		WHEN "0111" =>
+		--NOT A
+			preR(7 downto 0) <= NOT A;
+			HEX <= "0101011010001100001110001000";
+			
+		WHEN "1000" =>
+		--A
+			preR(7 downto 0) <= A;
+			HEX <= "1111111111111111111110001000";
+			
+		WHEN "1001" =>
+		--B
+			preR(7 downto 0) <= B;
+			HEX <= "1111111111111111111110000011";
+		WHEN "1010" =>
+		--A+1
+			preR <= A+"000000001";
+			preCo<= preR(8);
+			HEX <= "1111111000100000011111111001";
+			
+		WHEN "1011" =>
+		--A-1
+			preR <= A-"000000001";
+			preCo<= preR(8);
+			HEX <= "1111111000100001111111111001";
+			
+		WHEN "1100" =>
+		--A+B+Ci
+			preR <= A + B + Ci + "000000000";
+			preCo<= preR(8);
+			HEX <= "1111111000100000011110000011";
+			
+		WHEN "1101" =>
+		--A-B-Ci
+			preR <= A - B - Ci + "000000000";
+			preCo<= preR(8);
+			HEX <= "1111111000100001111110000011";
+			
+		WHEN "1110" =>
+			preR <= "011111111";
+			HEX <= "1111001111100111110011111001";
+		WHEN "1111" =>
+			preR <= "011111111";
+			HEX <= "1111001111100111110011111001";
+		END CASE;
+		
+		IF preR(7 downto 0) = "00000000" THEN Zo <= '1';
+		ELSE	Zo <= '0';
+		END IF;
+				
+		R <= preR(7 downto 0);
+		Co<= preCo;
+END PROCESS;
+END ARCHITECTURE;
+			
---
+++ ALU_CTRL.vhd
@@ -0,0 +1,126 @@
+library ieee;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+ENTITY ALU_CTRL IS
+	PORT(
+		clk:				in 	std_logic;								--reloj maestro del sistema
+		valueOutput:	out	std_logic_vector(7 downto 0);		--resultado de operación
+		Zout:				out	std_logic;								--zout
+		carryOut:		out 	std_logic;								--carry de salida 1 bit
+		cState:			out 	std_logic_vector(1 downto 0);
+		HEXOUT:			out   std_logic_vector(27 downto 0);   --leds salidas hex
+		RST: 				in  	std_logic
+	);
+END ENTITY;
+
+ARCHITECTURE archALU_CTRL OF ALU_CTRL IS
+	
+	TYPE status IS (progmemRead, moveToRegisters, resultToW);
+	
+	SIGNAL state:status:=progmemRead;
+	SIGNAL nState:status;
+	SIGNAL regA: std_logic_vector(7 downto 0);
+	SIGNAL regB: std_logic_vector(7 downto 0);
+	SIGNAL rValue: std_logic_vector(7 downto 0);
+	SIGNAL ZoReg: std_logic;
+	SIGNAL CoReg: std_logic;
+	SIGNAL CoBuffer: std_logic:='0';
+	SIGNAL opIn: std_logic_vector(3 downto 0);
+	SIGNAL hexReg: std_logic_vector(27 downto 0);
+	SIGNAL CarryInput: std_logic;
+	
+	SIGNAL W: std_logic_vector(7 downto 0);
+	SIGNAL regW: std_logic_vector(7 downto 0):="00000000";
+	SIGNAL PC: std_logic_vector(3 downto 0);
+	SIGNAL IR: std_logic_vector(11 downto 0):="000000000000";
+	SIGNAL S: std_logic_vector(3 downto 0):="0000";
+	SIGNAL dB: std_logic_vector(7 downto 0):="00000000";
+	SIGNAL dataReg: std_logic_vector(11 downto 0):="000000000000";
+	SIGNAL addrReg: std_logic_vector(3 downto 0):="0000";
+	
+BEGIN
+
+	XALU: ENTITY work.ALU PORT MAP(	A	=> regW,
+												B  => regB,
+												Ci	=> carryInput,
+												R	=> rValue,
+												Zo	=> ZoReg,
+												Co	=> CoReg,
+												Op => opIn,
+												HEX => hexReg
+											);
+											
+	XROM: ENTITY work.ROM PORT MAP(	addr => addrReg,
+												data => dataReg
+											);
+
+	PROCESS(state,carryInput,RST,PC)
+	BEGIN
+		IF(rst='0') THEN
+				PC<="0000";
+				W<="00000000";
+		ELSE
+			CASE state IS	
+				WHEN progmemRead =>
+					addrReg <= PC;		-- introduce a PC [addr:ROM] el valor del contador addrReg
+					IR <= dataReg;		-- lee de dataReg [data:ROM] la info y la introduce a IR
+					nState <= moveToRegisters;
+					CarryInput <= CoBuffer;
+
+				WHEN moveToRegisters =>
+					S <= IR(11 downto 8);		--separa instrucción y guarda en S
+					regB <= IR(7 downto 0);		--separa byte e introduce el byte en regB [B:ALU]
+					regW <= W;						--mueve el valor de W a regW [A:ALU]
+					opIn <= S;						--introduce a opIn [Op:ALU] la instrucción
+					nState <= resultToW;
+					CoBuffer <= CoReg;
+
+				WHEN resultToW =>
+					W <= rValue;					--mueve resultado, rValue [R:ALU] a W
+					nState <= progmemRead;
+					PC <= addrReg + '1';			--suma +1 a addrReg
+			END CASE;
+		END IF;
+	END PROCESS;
+	
+	Zout <= ZoReg;
+	CarryOut <= CoBuffer;
+	valueOutput <= W;
+	
+	PROCESS(rValue,state,ZoReg,CoReg,hexReg)
+	BEGIN
+		CASE state IS
+			WHEN progmemRead =>
+				HEXOUT<="1000001000100010001111111001";
+				cState<="01";
+			WHEN moveToRegisters =>
+				HEXOUT<="1000001000100010001110100100";
+				cState<="10";
+			WHEN resultToW =>
+				HEXOUT<="0101111000011000100101111111";
+				cState<="11";
+		END CASE;
+	END PROCESS;
+	
+--	PROCESS(RST)
+--	BEGIN
+--		IF (RST = '0') THEN
+--			W <= "00000000";
+--			PC <= "0000";
+--			state <= progmemRead;
+--		END IF;
+--	END PROCESS;
+	
+	
+	PROCESS(nState,clk,RST)
+	BEGIN
+		IF (RST = '0') THEN
+			state <= progmemRead;
+		ELSIF (clk'event AND clk = '0') THEN 
+			state <= nState;
+		END IF;
+	END PROCESS;
+END ARCHITECTURE;
+
+
