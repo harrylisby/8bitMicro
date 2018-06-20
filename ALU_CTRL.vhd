af2866bd19c5b736f09f830dd61172270994c177
harrylisby <harry@lisby.co>
2018-06-20 11:12:45 -0600

    Añadida RAM e instancia en CTRL

--- ALU_CTRL.vhd
+++ ALU_CTRL.vhd
@@ -5,19 +5,19 @@ use ieee.numeric_std.all;
 ENTITY ALU_CTRL IS
 	PORT(
 		clk: in	std_logic; --reloj maestro del sistema
-		valueOutput: out std_logic_vector(7 downto 0); --resultado de operación
-		Zout: out std_logic; --zout
-		carryOut: out std_logic; --carry de salida 1 bit
-		cState:	out std_logic_vector(1 downto 0);
-		HEXOUT:	out std_logic_vector(27 downto 0); --leds salidas hex
-		RST: in	std_logic
+		valueOutput: out std_logic_vector(7 downto 0):="00000000"; --resultado de operación
+		Zout: out std_logic:='0'; --zout
+		carryOut: out std_logic:='0'; --carry de salida 1 bit
+		cState:	out std_logic_vector(1 downto 0):="00";
+		HEXOUT:	out std_logic_vector(27 downto 0):="0000000000000000000000000000"; --leds salidas hex
+		RST: in	std_logic:='0'
 	);
 END ENTITY;
 
 ARCHITECTURE archALU_CTRL OF ALU_CTRL IS
-	
+--Estados
 	TYPE status IS (progmemRead, moveToRegisters, resultToW);
-	
+--Signals para ALU
 	SIGNAL state:status:=progmemRead;
 	SIGNAL nState:status;
 	SIGNAL regA: std_logic_vector(7 downto 0);
@@ -29,7 +29,7 @@ ARCHITECTURE archALU_CTRL OF ALU_CTRL IS
 	SIGNAL opIn: std_logic_vector(3 downto 0);
 	SIGNAL hexReg: std_logic_vector(27 downto 0);
 	SIGNAL CarryInput: std_logic;
-	
+--Signals para ROM
 	SIGNAL W: std_logic_vector(7 downto 0);
 	SIGNAL regW: std_logic_vector(7 downto 0):="00000000";
 	SIGNAL PC: std_logic_vector(3 downto 0);
@@ -38,10 +38,16 @@ ARCHITECTURE archALU_CTRL OF ALU_CTRL IS
 	SIGNAL dB: std_logic_vector(7 downto 0):="00000000";
 	SIGNAL dataReg: std_logic_vector(11 downto 0):="000000000000";
 	SIGNAL addrReg: std_logic_vector(3 downto 0):="0000";
+--signals para RAM
+	SIGNAL RWRR: std_logic; --ram write/read register
+	SIGNAL RADDR: std_logic_vector(6 downto 0); --ram address register;
+	SIGNAL RDIR: std_logic_vector(7 downto 0); --ram data in register;
+	SIGNAL RDOR: std_logic_vector(7 downto 0); --ram data out register;
 	
 BEGIN
 
-	XALU: ENTITY work.ALU PORT MAP(	A => regW,
+	XALU: ENTITY work.ALU PORT MAP(
+					A => regW,
 					B  => regB,
 					Ci => carryInput,
 					R => rValue,
@@ -49,13 +55,21 @@ BEGIN
 					Co => CoReg,
 					Op => opIn,
 					HEX => hexReg
-				      );
+	);
 											
-	XROM: ENTITY work.ROM PORT MAP(	addr => addrReg,
+	XROM: ENTITY work.ROM PORT MAP(
+					addr => addrReg,
 					data => dataReg
-				      );
+	);
+						
+	XRAM: ENTITY work.RAM PORT MAP(
+					WR => RWRR,
+					ADDR => RADDR,
+					DATA_IN => RDIR,
+					DATA_OUT => RDOR
+	);
 
-	PROCESS(state,carryInput,RST,PC)
+	PROCESS(state,carryInput,RST,PC,IR,dataReg,carryInput)
 	BEGIN
 		IF(rst='0') THEN
 				PC<="0000";
@@ -87,31 +101,6 @@ BEGIN
 	Zout <= ZoReg;
 	CarryOut <= CoBuffer;
 	valueOutput <= W;
-	
-	PROCESS(rValue,state,ZoReg,CoReg,hexReg)
-	BEGIN
-		CASE state IS
-			WHEN progmemRead =>
-				HEXOUT<="1000001000100010001111111001";
-				cState<="01";
-			WHEN moveToRegisters =>
-				HEXOUT<="1000001000100010001110100100";
-				cState<="10";
-			WHEN resultToW =>
-				HEXOUT<="0101111000011000100101111111";
-				cState<="11";
-		END CASE;
-	END PROCESS;
-	
---	PROCESS(RST)
---	BEGIN
---		IF (RST = '0') THEN
---			W <= "00000000";
---			PC <= "0000";
---			state <= progmemRead;
---		END IF;
---	END PROCESS;
-	
 	
 	PROCESS(nState,clk,RST)
 	BEGIN
